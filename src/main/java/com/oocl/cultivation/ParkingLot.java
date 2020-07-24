c0ff6cce3ce1c0b60fa3d22edca0a2e574b8f464
userName <gongl3730@gmail.com>
2020-07-24 15:28:57 +0800

    [ParkingLot][Bruno] feature: add parkingLot class and add park method

---
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -0,0 +1,11 @@
+package com.oocl.cultivation;
+
+public class ParkingLot {
+
+    private Car car;
+    private CarTicket carTicket;
+
+    public CarTicket park(Car car) {
+        return null;
+    }
+}
