da7c8ceaf735d6be429c306f4ddbfb8a5919d432
Bruno8310 <gongl3730@gmail.com>
2020-07-25 09:21:42 +0800

    [parkingLot][Bruno] feature: implemnt judge parkingLot capacity then park car

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -7,8 +7,11 @@ public class ParkingLot {
 
     public Map<CarTicket, Car> carTicketCarMap;
 
+    private int capacity;
+
     public ParkingLot() {
         this.carTicketCarMap = new HashMap<>();
+        this.capacity = 10;
     }
 
     public CarTicket park(Car car) {
@@ -19,5 +22,14 @@ public class ParkingLot {
 
     public Car fetch(CarTicket carTicket) {
         return this.carTicketCarMap.remove(carTicket);
+    }
+
+    public CarTicket judgeCapacityPark(Car car) {
+        if (this.carTicketCarMap.size() <= this.capacity) {
+           CarTicket carTicket = this.park(car);
+           this.capacity--;
+           return carTicket;
+        }
+        return null;
     }
 }
