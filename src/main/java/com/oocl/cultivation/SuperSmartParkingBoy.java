e0d99cb0a53f7eeb057a7b9f1e95e785c9937b04
Bruno8310 <gongl3730@gmail.com>
2020-07-26 15:42:44 +0800

    [parkingLot][Bruno] feature: implement judgeParkingCapacity and sort by capacity rate

--- src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -13,6 +13,21 @@ public class SuperSmartParkingBoy extends ParkingBoy {
     @Override
     public CarTicket judgeCapacityPark(Car car) {
 
-       return null;
+        Collections.sort(this.getParkingLotList(), new Comparator<ParkingLot>() {
+            @Override
+            public int compare(ParkingLot o1, ParkingLot o2) {
+                return (o1.getCapacity() - o1.getCarTicketCarMap().size()) / o1.getCapacity() - (o2.getCapacity() - o2.getCarTicketCarMap().size()) / o2.getCapacity();
+            }
+        });
+
+        ParkingLot firstparkingLot = this.getParkingLotList().get(0);
+        if (this.getParkingLot().getCapacity() > 0) {
+            CarTicket carTicket = this.park(car);
+            this.getParkingLot().setCapacity();
+            return carTicket;
+        } else {
+            this.setResponseMessage("Not enough position.");
+            return null;
+        }
     }
 }
