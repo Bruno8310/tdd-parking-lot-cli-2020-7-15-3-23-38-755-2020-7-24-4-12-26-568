fd4b1f700d0a91a7c5691a151cd0b69a817b16c9
Bruno8310 <gongl3730@gmail.com>
2020-07-27 20:59:03 +0800

    [story4][Bruno] feature: add

--- src/main/java/com/oocl/cultivation/SmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -3,8 +3,17 @@ package com.oocl.cultivation;
 
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
-public class SmartParkingBoy {
+public class SmartParkingBoy extends ParkingBoy {
 
 
+    public SmartParkingBoy(List<ParkingLot> parkingLots) {
+        super(parkingLots);
+    }
+
+    @Override
+    public CarTicket park(Car car) {
+        return super.park(car);
+    }
 }
--- src/test/java/com/oocl/cultivation/test/SmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/SmartParkingBoyTest.java
@@ -1,15 +1,30 @@
 package com.oocl.cultivation.test;
 
 import com.oocl.cultivation.Car;
+import com.oocl.cultivation.ParkingBoy;
 import com.oocl.cultivation.ParkingLot;
 import com.oocl.cultivation.SmartParkingBoy;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SmartParkingBoyTest {
     @Test
-    void should_park_cars_when_parkingLot_hava_max_position_give_parkingLot_list() {
-
+    void should_return_first_parkingLot_size_1_when_parkingLot_hava_max_position_give_2_parkingLot_and_3_cars() {
+        // given
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            parkingLots.add(new ParkingLot());
+        }
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        // when
+        for (int j = 0; j < 3; j++) {
+            smartParkingBoy.park(new Car());
+        }
+        // then
+        assertEquals(1, smartParkingBoy.getParkingLot().getCarTicketCarMap().size());
     }
 }
