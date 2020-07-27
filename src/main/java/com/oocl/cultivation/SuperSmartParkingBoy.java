125f5d05c7112dcd614d4e57fc293211f28e7751
Bruno8310 <gongl3730@gmail.com>
2020-07-27 21:23:28 +0800

    [story5][Bruno] feature: add should_select_large_position_rate_to_parking_when_parking_cars_give_car_list_and_parkingLot_list method

--- src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -3,7 +3,17 @@ package com.oocl.cultivation;
 
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
-public class SuperSmartParkingBoy {
+public class SuperSmartParkingBoy extends ParkingBoy{
 
+
+    public SuperSmartParkingBoy(List<ParkingLot> parkingLots) {
+        super(parkingLots);
+    }
+
+    @Override
+    public CarTicket park(Car car) {
+        return super.park(car);
+    }
 }
--- src/test/java/com/oocl/cultivation/test/SuperSmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/SuperSmartParkingBoyTest.java
@@ -2,15 +2,31 @@ package com.oocl.cultivation.test;
 
 import com.oocl.cultivation.Car;
 import com.oocl.cultivation.ParkingLot;
+import com.oocl.cultivation.SmartParkingBoy;
 import com.oocl.cultivation.SuperSmartParkingBoy;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SuperSmartParkingBoyTest {
 
     @Test
-    void should_park_cars_when_parkingLot_hava_large_position_rate_give_parkingLot_list() {
-
+    void should_select_large_position_rate_to_parking_when_parking_cars_give_car_list_and_parkingLot_list() {
+        // given
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            parkingLots.add(new ParkingLot());
+        }
+        SuperSmartParkingBoy supertParkingBoyarkingBoy = new SuperSmartParkingBoy(parkingLots);
+        // when
+        for (int j = 0; j < 3; j++) {
+            supertParkingBoyarkingBoy.park(new Car());
+        }
+        // then
+        assertEquals(2, supertParkingBoyarkingBoy.getParkingLotList().get(0).getCarTicketCarMap().size());
+        assertEquals(1, supertParkingBoyarkingBoy.getParkingLotList().get(1).getCarTicketCarMap().size());
     }
 }
