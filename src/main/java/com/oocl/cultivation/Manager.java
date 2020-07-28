31001e5369081a5d4663aaaf22c5dc2576eed1a3
userName <gongl3730@gmail.com>
2020-07-28 09:19:00 +0800

    [story6][Bruno] feature: add manager class and addParkingBoyList method

---
+++ src/main/java/com/oocl/cultivation/Manager.java
@@ -0,0 +1,8 @@
+package com.oocl.cultivation;
+
+public class Manager {
+    public int addParkingBoyList(ParkingBoy parkingBoy) {
+
+        return 0;
+    }
+}
---
+++ src/test/java/com/oocl/cultivation/test/ServiceManagerTest.java
@@ -0,0 +1,31 @@
+package com.oocl.cultivation.test;
+
+import com.oocl.cultivation.Manager;
+import com.oocl.cultivation.ParkingBoy;
+import com.oocl.cultivation.ParkingLot;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ServiceManagerTest {
+
+    @Test
+    void should_return_parkingBoy_id_when_add_parkingBoy_give_manager_and_parkingBoy() {
+        // given
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            parkingLots.add(new ParkingLot());
+        }
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
+        Manager manager = new Manager();
+
+        // when
+        int result = manager.addParkingBoyList(parkingBoy);
+
+        // then
+        assertEquals(1, result);
+    }
+}
